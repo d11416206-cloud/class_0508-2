38340ccc7ac23bd6ef9d18270bb8f129fc9859c6
d11416206-cloud <d11416206@gs.takming.edu.tw>
2026-05-08 12:08:06 +0800

    Add files via upload

---
+++ Class1.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Security.Cryptography.X509Certificates;
+
+public class Person
+{
+	public string GetName()
+	{
+
+	}
+}
+public string Student()
+{
+	Public string GetName()
+	{
+
+	}
+    
+}
+public string Teacher()
+{
+    Public string GetName()
+    {
+
+    }
+
+}
+
+
+public interface IName
+{
+    string GetName();
+}
+
+public class Person : IName
+{
+    public string GetName()
+    {
+        return "Person";
+    }
+}
+
+public class Student : IName
+
+{
+    public string GetName()
+    {
+        return "Student";
+    }
+}
+
+public class Caller
+{
+    Public void CallName(IName a)
+    {
+        Console.WriteLine(a);
+    }
+
+}
+
---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System.Runtime.CompilerServices;
+
+namespace class_0508_2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Caller call = new Caller();
+
+            call.CallName(new Person());
+            call.CallName(new Student());
+        }
+    }
+}
